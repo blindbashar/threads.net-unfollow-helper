a814f4ebd8643725b174886db52b55797f817b1a
blindbashar <to@iaminteractive.net>
2025-03-19 17:11:24 -0700

    Updated script to remove delay; refactored code; added cross-platform browser testing info

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
+## Threads.net Unfollow Helper Script
+
+# Use
+1. Navigate to www.threads.net and log in
+2. Click on "#,### followers" in your bio
+3. Switch to the 'Following' tab
+4. Paste this script into your browser's dev console
+5. Manually click through the stack of 'Unfollow' bubbles
+6. Scroll down the 'Following' list to load more profiles
+7. Repeat
+
+### Tested and works on Firefox and Chrome.
+
+## ORIGINAL README
+# https://github.com/sanderbell/unfollow-on-threads
+
 # Mass Unfollow on Threads.net
 
 ## Overview
--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-// Find all focusable `div` elements (except those with `tabindex="-1"`)
-const buttons = document.querySelectorAll('div[tabindex]:not([tabindex="-1"])');
-// Loop through each found button
-for (let button of buttons) {
-  // Check if the button's text contains "Following"
-  if (button.textContent.includes('Following')) {
-    // Click the button
-    button.click();
-    // Wait for 500 milliseconds (simulated delay)
-    await new Promise((resolve) => setTimeout(resolve, 500));
-  }
+// Use: Open the 'Following' menu from your Threads profile,
+//		paste & run this script in the developer console,
+//		then manually click through the stack of 'Unfollow' bubbles
+//		and scroll down to load a new batch of profiles.
+//		Repeat.
+async function clickButtonsWithDelay(selector, textToMatch) {
+	// Select all button elements from the DOM
+	let buttons = document.querySelectorAll(selector);
+	for (let button of buttons) {
+		// See if the button matches our text
+		if (button.textContent.includes(textToMatch)) {
+			button.click();
+		}
+	}
 }
+
+// Run the script, clicking all 'Following' buttons on the page.
+await clickButtonsWithDelay('div[tabindex]:not([tabindex="-1"])', 'Following');
